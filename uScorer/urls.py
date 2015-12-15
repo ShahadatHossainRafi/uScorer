1fdeb1ed4888ccc7d4a17670c068e5db62508ff5
Shahadat Hossain <rafi@in2rafi.com>
2015-12-16 01:00:51 +0600

    home and about pages created

--- uScorer/urls.py
+++ uScorer/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from uScorer.views import home
+from uScorer.views import about
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', home),
+    url(r'^about/$', about),
 ]
